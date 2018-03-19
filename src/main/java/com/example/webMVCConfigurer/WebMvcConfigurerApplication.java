2229cf1e614f85f1f3fc57f5ccc122901781e322
tikTokTik <mithoonk@flock.com>
2018-03-19 15:18:57 +0530

    Add files

---
+++ src/main/java/com/example/webMVCConfigurer/WebMvcConfigurerApplication.java
@@ -0,0 +1,12 @@
+package com.example.webMVCConfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcConfigurerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcConfigurerApplication.class, args);
+	}
+}
